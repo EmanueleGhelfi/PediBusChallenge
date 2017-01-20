f72550dd75144e42146689e3054285e9db970ea0
Emanuele Ghelfi <manughelfi1994@gmail.com>
2017-01-20 09:57:06 +0100

    -- commit

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,28 @@
-import com.ampl.Entity;
-import com.ampl.Objective;
-import com.ampl.Tuple;
+import com.ampl.*;
+import com.sun.xml.internal.rngom.digested.DDataPattern;
+
+import java.io.IOException;
 
 /**
  * Created by emanueleghelfi on 20/01/2017.
  */
 public class Test {
 
-    Objective obj;
+    public static void main(String[] args){
+
+        AMPL ampl  = new AMPL();
+        try {
+            ampl.readData("../pedibus_10.dat");
+            Parameter alpha =ampl.getParameter("alpha");
+            System.out.println(alpha);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
 }
