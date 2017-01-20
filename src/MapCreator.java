481a511efada5d03502db55e841c6a58011902ca
Emanuele Ghelfi <manughelfi1994@gmail.com>
2017-01-20 17:15:03 +0100

    -- commit

--- src/MapCreator.java
+++ src/MapCreator.java
@@ -5,32 +5,39 @@ import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by emanueleghelfi on 20/01/2017.
  */
 public class MapCreator {
 
-    private WeightedGraph<Node, DefaultEdge> weightedGraph;
+    private WeightedGraph<Vertex, Arc> weightedGraph;
 
-    public WeightedGraph<Node,DefaultEdge> getUndirectedGraph(){
+    public WeightedGraph<Vertex,Arc> getUndirectedGraph(){
         return weightedGraph;
     }
 
     public MapCreator(){
-        weightedGraph = new SimpleWeightedGraph<Node, DefaultEdge>(DefaultEdge.class);
+        weightedGraph = new SimpleWeightedGraph<Vertex, Arc>(Arc.class);
     }
 
-    public void addNode(Node node){
+    public void addNode(Vertex node){
         weightedGraph.addVertex(node);
     }
 
     public void createGraph(){
 
-        List<Node> alreadyVisitedNodes = new ArrayList<Node>();
-        for(Node node: weightedGraph.vertexSet()){
+        List<Vertex> alreadyVisitedNodes = new ArrayList<Vertex>();
+        for(Vertex node: weightedGraph.vertexSet()){
             alreadyVisitedNodes.add(node);
-            for(Node toNode: weightedGraph)
+            for(Vertex toNode: weightedGraph.vertexSet()){
+                if(alreadyVisitedNodes.contains(toNode)){
+                    Arc arc = new Arc(9);
+                    weightedGraph.addEdge(node,toNode,arc);
+                    //weightedGraph.setEdgeWeight(arc,);
+                }
+            }
 
 
         }
